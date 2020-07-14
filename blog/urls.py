0c641416e4dac09d1353697165216e4d5a28f262
admin <admin@admin.com>
2020-07-14 23:11:35 +0800

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    #views.post_list意味着要在view.py中创建post_list视图
+    #name设置的是url的名字
     url(r'^$', views.post_list, name='post_list'),
+
+    #(?P<pk>[0-9]+)表示将[0-9]+中的内容放入到变量pk中，这个变量可以传参到post_detail视图中
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #blog/post_list.html意味着要创建post_list.html模板
+    #{'posts': posts}意味着'posts'中的posts这个名字可以在html意味着要创建post_list.html模板中使用
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+#pk是在urls.py中设置的参数
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+
 
 
 
